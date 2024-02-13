d4ea18a232bd45643149651adfb7d377e2a30f55
erdilhasan <hasanerdil1260@gmail.com>
2024-02-13 21:58:19 +0200

    gitgnore

--- src/firebase.jsx
+++
@@ -1,9 +0,0 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-
-const firebaseConfig = {
-  // Your Config?
-};
-
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
